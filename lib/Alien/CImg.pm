29f8c126e97a043b3d46fcfd6c7a7d19f681cf39
Kang-min Liu <gugod@gugod.org>
2016-08-25 17:24:27 +0200

    some release info

--- lib/Alien/CImg.pm
+++ lib/Alien/CImg.pm
@@ -3,4 +3,17 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 use parent 'Alien::Base';
+
 1;
+
+__END__
+
+=head1 NAME
+
+Alien::CImg - CImg distribution for CPAN.
+
+=head1 AUTHOR
+
+Kang-min Liu <gugod@gugod.org>
+
+=cut
