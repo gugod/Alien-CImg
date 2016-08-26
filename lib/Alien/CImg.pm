08d8b5f50f80bdcffe4b742f487c3d836f23cb51
Kang-min Liu <gugod@gugod.org>
2016-08-26 11:04:16 +0200

    some documentation.

--- lib/Alien/CImg.pm
+++ lib/Alien/CImg.pm
@@ -12,6 +12,22 @@ __END__
 
 Alien::CImg - CImg distribution for CPAN.
 
+=head1 Description
+
+Ouote from the L<cimg website|http://cimg.eu/>
+
+=over 4
+
+CImg stands for Cool Image : It is easy to use, efficient and is
+intended to be a very pleasant toolbox to design image processing
+algorithms in C++. Due to its generic conception, it can cover a wide
+range of image processing applications.
+
+=back
+
+The CImg distribution is only a .h file CImg.h, the way you use it is
+to just include it in your C/C++ source code.
+
 =head1 AUTHOR
 
 Kang-min Liu <gugod@gugod.org>
