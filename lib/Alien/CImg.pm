2c7b9ff572e3e2c6f424911e64dfb262755ab235
Kang-min Liu <gugod@gugod.org>
2016-08-26 11:25:43 +0200

    more documentation.

--- lib/Alien/CImg.pm
+++ lib/Alien/CImg.pm
@@ -14,7 +14,12 @@ Alien::CImg - CImg distribution for CPAN.
 
 =head1 Description
 
-Ouote from the L<cimg website|http://cimg.eu/>
+This is a Alien distribution based on L<Alien::Base>, class methods
+such as C<cflags> and C<libs> are inherited from there. See the
+documentation for L<Alien::Base> for full list of methods. This module
+does not override or extend any of those inherited methods.
+
+Quote from the L<cimg website|http://cimg.eu/>
 
 =over 4
 
@@ -27,6 +32,26 @@ range of image processing applications.
 
 The CImg distribution is only a .h file CImg.h, the way you use it is
 to just include it in your C/C++ source code.
+
+=over 4
+
+#inculed <CImg.h>
+
+=back
+
+If you are authoring a CPAN distribution that requires CImg, you could
+get the cflags by running this L<palien|App::palien> command:
+
+   palien --cflags Alien::CImg
+
+Or alternativly, this one-liner:
+
+   perl -MAlien::CImg -E 'say Alien::CImg->cflags'
+
+Since CImg installation only contains source code, it does not require
+linking. So there is no need for a C<-lcimg> command flag.
+
+See also L<palien|App::palien> and L<Alien::Base>
 
 =head1 AUTHOR
 
